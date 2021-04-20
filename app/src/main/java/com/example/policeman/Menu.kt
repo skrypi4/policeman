28561e2023f89a8c95032ef75437dfd2e948be56
skrypi4 <gonshikx@gmail.com>
2021-04-20 12:36:24 +0500

    анимка

--- app/src/main/java/com/example/policeman/Menu.kt
+++ app/src/main/java/com/example/policeman/Menu.kt
@@ -3,6 +3,9 @@ package com.example.policeman
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import androidx.navigation.Navigation
 
@@ -17,29 +20,49 @@ class Menu : AppCompatActivity() {
         val menuButton4 = findViewById<Button>(R.id.button4)
         val menuButton5 = findViewById<Button>(R.id.button5)
 
-        menuButton5.setOnClickListener {
-            val intent = Intent(this@Menu, aboutUs::class.java)
-            startActivity(intent)
+        val mTranslateAnimation = AnimationUtils.loadAnimation(this, R.anim.translate_inviz)
+
+        menuButton1.setOnClickListener {
+            menuButton1.startAnimation(mTranslateAnimation)
+            val handler1 = Handler().postDelayed(Runnable {
+                val intent = Intent(this@Menu, department::class.java)
+                startActivity(intent)
+            }, 600)
         }
 
         menuButton2.setOnClickListener {
-            val intent = Intent(this@Menu, dlyaFramentov::class.java)
-            startActivity(intent)
-        }
-
-            menuButton1.setOnClickListener {
-                val intent = Intent(this@Menu, department::class.java)
+            menuButton2.startAnimation(mTranslateAnimation)
+            val handler1 = Handler().postDelayed(Runnable {
+                val intent = Intent(this@Menu, dlyaFramentov::class.java)
                 startActivity(intent)
+            }, 600)
         }
 
         menuButton3.setOnClickListener {
-            val intent = Intent(this@Menu, photoRobot::class.java)
-            startActivity(intent)
+            menuButton3.startAnimation(mTranslateAnimation)
+            val handler1 = Handler().postDelayed(Runnable {
+                val intent = Intent(this@Menu, photoRobot::class.java)
+                startActivity(intent)
+            }, 600)
         }
 
         menuButton4.setOnClickListener {
-            val intent = Intent(this@Menu, Paint::class.java)
-            startActivity(intent)
+            menuButton4.startAnimation(mTranslateAnimation)
+            menuButton4.visibility = View.INVISIBLE
+            val handler1 = Handler().postDelayed(Runnable {
+                val intent = Intent(this@Menu, Paint::class.java)
+                startActivity(intent)
+            }, 750)
         }
+
+        menuButton5.setOnClickListener {
+            menuButton5.startAnimation(mTranslateAnimation)
+            val handler1 = Handler().postDelayed(Runnable {
+                val intent = Intent(this@Menu, aboutUs::class.java)
+                startActivity(intent)
+            }, 750)
+
+        }
+
     }
 }
